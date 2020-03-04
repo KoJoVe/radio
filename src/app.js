3dfe672767a61e1d3677f67e9017625c130a6d97
Guedes <joao.vicente.guedes@accenture.com>
2020-03-04 13:57:55 -0300

    adds http-server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.png';
+import beetle from './beetle.jpg';
 import './app.css';
 
 const App = () => {
@@ -7,7 +8,10 @@ const App = () => {
     <div className="app">
       <div className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
-        <h2>welcome to <span className="red">Besouro</span> radio</h2>
+        <h3>coming soon:</h3>
+        <h2>prepare to <span className="red">Besouro</span> radio</h2>
+        <img src={beetle} className="beetle" alt="logo" />
+        <h5>Techno, House, Breakbeat <br /> Deep, Melodic and +</h5>
       </div>
     </div>
   );
