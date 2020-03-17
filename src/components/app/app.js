403d9bed4820732fcd77d4c78a371242bf4fa564
Cristovao Pereira de Souza <joaovngg@gmail.com>
2020-03-17 16:38:03 -0300

    color scheme :D

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,15 +10,17 @@ const App = () => {
         <div className="app">
             <div className="main">
                 <img src={logo} className={`app-logo ${playing ? "animated" : ""}`} alt="logo" />
-                <h2>welcome to <span className="red">Besouro</span> radio</h2>
-                <h5>make sure to check out our timetable so you can know when livestreams will happen ;)</h5>
-                <div style={{ animation: playing ? `beetle-dance infinite ${1000}ms linear` : ''}} className={`beetle ${playing ? "animated" : ""}`} onClick={() => setPlaying(!playing)}></div>
-                <h3>current status is: <span className="red">offline</span></h3>
-                <h5>click on the beetle to play/pause</h5>
+                <h2 className="dark">welcome to <span className="pink">Besouro</span> radio</h2>
+                <h5 className="blue">make sure to check out our timetable so you can know when livestreams will happen ;)</h5>
+                <div style={{ animation: playing ? `beetle-dance infinite ${1000}ms linear` : ''}} className={`beetle ${playing ? "animated" : ""}`} onClick={() => setPlaying(!playing)}>
+                    <div className="playButton"></div>
+                </div>
+                <h3 className="dark">current status is: <span className="dark">offline</span></h3>
+                <h5 className="blue">click on the beetle to play/pause</h5>
             </div>
             <Player playing={playing} />
             <div className="schedule">
-                <h3>Our Schedule:</h3>
+                <h3 className="green">our timetable:</h3>
                 <ul>
                     <li>João Vicente - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b> (BRT)</li>
                     <li>Samuel Bastos - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b> (BRT)</li>
