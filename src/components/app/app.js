f184ca766de08aa446bcdce6a73bbfa8c99b4b2c
Cristovao Pereira de Souza <joaovngg@gmail.com>
2020-03-17 02:28:14 -0300

    adds space

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -20,10 +20,10 @@ const App = () => {
             <div className="schedule">
                 <h3>Our Schedule:</h3>
                 <ul>
-                    <li>João Vicente - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b>(BRT)</li>
-                    <li>Samuel Bastos - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b>(BRT)</li>
-                    <li>Tadeu Estanislau - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b>(BRT)</li>
-                    <li>Lucas Rapini - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b>(BRT)</li>
+                    <li>João Vicente - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b> (BRT)</li>
+                    <li>Samuel Bastos - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b> (BRT)</li>
+                    <li>Tadeu Estanislau - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b> (BRT)</li>
+                    <li>Lucas Rapini - Every <b>month</b> on the <b>1st</b> and <b>15th</b> <b>21:00</b> (BRT)</li>
                 </ul>
             </div>
         </div>
