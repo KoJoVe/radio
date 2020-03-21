56bfe50cbdc3f25d01f633bc131ccc7221af0ba9
Cristovao Pereira de Souza <joaovngg@gmail.com>
2020-03-21 03:36:55 -0300

    test mediasession metadata

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -24,13 +24,25 @@ const Player = (props) => {
             window.postMessage('twitchonline', window.location.origin);
         });
 
-        window.navigator.mediaSession.setActionHandler('play', () => {
-            newPlayer.play();
+        window.navigator.mediaSession.metadata = new MediaMetadata({
+            title: 'Besouro Live',
+            artwork: [
+                { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
+                { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+                { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+                { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+                { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+                { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+            ]
         });
 
-        window.navigator.mediaSession.setActionHandler('pause', () => {
-            newPlayer.pause();
-        });
+        // window.navigator.mediaSession.setActionHandler('play', () => {
+        //     newPlayer.play();
+        // });
+
+        // window.navigator.mediaSession.setActionHandler('pause', () => {
+        //     newPlayer.pause();
+        // });
     }, []);
 
     useEffect(() => {
