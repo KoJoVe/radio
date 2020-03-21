1451f07dc54b6be5c90c96b0f5fe4a5511cf1254
Cristovao Pereira de Souza <joaovngg@gmail.com>
2020-03-21 01:25:49 -0300

    player status

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Player from '../player/player';
 import logo from '../../assets/logo.png';
 import volumeup from '../../assets/volumeup.svg';
@@ -6,12 +6,33 @@ import volumeoff from '../../assets/volumeoff.svg';
 import './app.css';
 
 const App = () => {
+    const [online, setOnline] = useState(null);
     const [playing, setPlaying] = useState(false);
     const [schedule, setSchedule] = useState(false);
     const [volume, setVolume] = useState(0.5);
 
+    useEffect(() => {
+        window.addEventListener('message', (event) => {
+            if (event.data === 'twitchoffline') {
+                setOnline(false);
+            } else if (event.data === 'twitchonline') {
+                setOnline(true);
+            }
+        }, false);
+    }, []);
+
     const handleChange = (event) => {
         setVolume(event.target.value);
+    }
+
+    const getOnline = () => {
+        if (online === true) {
+            return (<span className="green">online</span>); 
+        } else if (online === false) {
+            return (<span className="dark">offline</span>);
+        } else {
+            return (<span className="dark">loading...</span>);
+        }
     }
 
     return (
@@ -24,21 +45,22 @@ const App = () => {
                     <div className="playButton"></div>
                 </div>
                 <h5 className="blue">click on the beetle to play/pause</h5>
-                <h3 className="dark">current status is: <span className="dark">offline</span></h3>
+                <h3 className="dark">current status is: <span className="dark">{ getOnline() }</span></h3>
                 <div className={`volume ${playing ? "expanded" : ""}`}>
                     <img src={volumeoff} /><input type="range" min="0" max="1" step="0.01" value={volume} onChange={handleChange}></input><img src={volumeup} />
                 </div>
                 {
-                    !schedule ? null :
-                    <div className="schedule">
-                        <a><a href="https://soundcloud.com/lucas-rapini">João Vicente</a><b>1st</b> & <b>3rd</b> <b>Tuesday</b><b> 21:00</b> (BRT)</a>
-                        <a><a href="https://soundcloud.com/lucas-rapini">Tadeu Estanislau</a><b>1st</b> & <b>3rd</b> <b>Thursday</b><b> 21:00</b> (BRT)</a>
-                        <a><a href="https://soundcloud.com/lucas-rapini">Samuel Bastos</a><b>2nd</b> & <b>4th</b> <b>Tuesday</b><b> 21:00</b> (BRT)</a>
-                        <a><a href="https://soundcloud.com/lucas-rapini">Lucas Rapini</a><b>2nd</b> & <b>4th</b> <b>Thursday</b><b> 21:00</b> (BRT)</a>
-                    </div>
+                    playing ? null :
+                    <h4 className="showSchedule blue" onClick={() => setSchedule(!schedule)}><span className="pink">click</span> to see our schedule</h4>
                 }
+                <div className={`schedule ${schedule && !playing ? "expanded" : ""}`}>
+                    <a><a href="https://soundcloud.com/lucas-rapini">João Vicente</a><b>1st</b> & <b>3rd</b> <b>Tuesday</b><b> 21:00</b> (BRT)</a>
+                    <a><a href="https://soundcloud.com/lucas-rapini">Tadeu Estanislau</a><b>1st</b> & <b>3rd</b> <b>Thursday</b><b> 21:00</b> (BRT)</a>
+                    <a><a href="https://soundcloud.com/lucas-rapini">Samuel Bastos</a><b>2nd</b> & <b>4th</b> <b>Tuesday</b><b> 21:00</b> (BRT)</a>
+                    <a><a href="https://soundcloud.com/lucas-rapini">Lucas Rapini</a><b>2nd</b> & <b>4th</b> <b>Thursday</b><b> 21:00</b> (BRT)</a>
+                </div>
             </div>
-            <div className="player">
+            <div className="player" id="theplayer">
                 <Player playing={playing} volume={volume}/>
             </div>
         </div>
--- src/components/player/player.js
+++ src/components/player/player.js
@@ -8,13 +8,21 @@ const Player = (props) => {
         const options = {
             width: '100%',
             height: '100%',
-            autoplay: true,
-            channel: "lucasrapini",
+            autoplay: false,
+            channel: "kojovee",
         };
         const newPlayer = new window.Twitch.Player("player", options);
         newPlayer.setVolume(0.5);
         newPlayer.pause();
         setPlayer(newPlayer);
+
+        newPlayer.addEventListener(window.Twitch.Player.OFFLINE, () => {
+            window.postMessage('twitchoffline', window.location.origin);
+        });
+
+        newPlayer.addEventListener(window.Twitch.Player.ONLINE, () => {
+            window.postMessage('twitchonline', window.location.origin);
+        });
 
         window.navigator.mediaSession.setActionHandler('play', () => {
             player.play();
