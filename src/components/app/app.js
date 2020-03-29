bcac5de73ece0ff977cbc462c984b66b56f4f00d
Cristovao Pereira de Souza <joaovngg@gmail.com>
2020-03-29 20:15:49 -0300

    removes the damn twitch and make the site simpler

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,27 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Player from '../player/player';
 import logo from '../../assets/logo.png';
-import volumeup from '../../assets/volumeup.svg';
-import volumeoff from '../../assets/volumeoff.svg';
 import './app.css';
 
 const App = () => {
     const [online, setOnline] = useState(null);
-    const [playing, setPlaying] = useState(false);
+    const [open, setOpen] = useState(true);
     const [schedule, setSchedule] = useState(false);
-    const [volume, setVolume] = useState(0.5);
 
-    const handleChange = (event) => {
-        setVolume(event.target.value);
-    }
-
-    const play = () => {
-        if (online) {
-            setPlaying(!playing);
-        } else {
-            setPlaying(false);
-        }
-    }
+    useEffect(() => {
+        axios.get('https://mixer.com/api/v1/channels/besouroradio', {})
+          .then((response) => {
+            setOnline(response.data && response.data.online);
+          })
+          .catch(() => {
+            setOnline(false);
+          });
+    }, []);
 
     const getOnline = () => {
         if (online === true) {
@@ -33,40 +29,25 @@ const App = () => {
         }
     }
 
-    useEffect(() => {
-        window.addEventListener('message', (event) => {
-            if (event.data === 'twitchoffline') {
-                setOnline(false);
-                setPlaying(false);
-            } else if (event.data === 'twitchonline') {
-                setOnline(true);
-            }
-        }, false);
-    }, []);
-
     return (
         <div className="app">
-            <div className="main">
-                <img src={logo} className={`app-logo ${playing ? "animated" : ""}`} alt="logo" />
+            <div className={`main ${!open ? "closed" : ""}`} >
+                <img src={logo} className={`app-logo ${online ? "animated" : ""}`} alt="logo" />
                 <h2 className="dark">welcome to <span className="pink">Besouro</span></h2>
                 <h5 className="blue">electronic music online radio</h5>
-                <div style={{ animation: playing ? `beetle-dance infinite ${1000}ms linear` : ''}} className={`beetle ${playing ? "animated" : ""}`} onClick={play}>
-                    <div className="playButton"></div>
-                </div>
+                <div style={{ animation: online ? `beetle-dance infinite ${1000}ms linear` : ''}} className={`beetle ${online ? "animated" : ""}`} onClick={() => setOpen(false)}></div>
                 <h3 className="dark">current status is: <span className="dark">{ getOnline() }</span></h3>
                 {
                     online ?
-                    <h5 className="blue">click on the beetle to play/pause</h5> :
+                    <h5 className="blue">click on the beetle to close the card</h5> :
                     <h5 className="blue">check our schedule below for live dates</h5>
                 }
-                <div className={`volume ${playing ? "expanded" : ""}`}>
-                    <img src={volumeoff} /><input type="range" min="0" max="1" step="0.01" value={volume} onChange={handleChange}></input><img src={volumeup} />
-                </div>
                 {
-                    playing ? null :
-                    <h4 className="showSchedule dark" onClick={() => setSchedule(!schedule)}><span className="pink">click</span> to see our schedule</h4>
+                    open ?
+                    <h4 className="showSchedule dark" onClick={() => setSchedule(!schedule)}><span className="pink">click</span> to see our schedule</h4> :
+                    <h4 className="showSchedule dark" onClick={() => setOpen(true)}><span className="pink">click</span> to reopen card</h4>
                 }
-                <div className={`schedule ${schedule && !playing ? "expanded" : ""}`}>
+                <div className={`schedule ${schedule ? "expanded" : ""}`}>
                     <span><a href="https://soundcloud.com/lucas-rapini">João Vicente</a><b>1st</b> & <b>3rd</b> <b>Tuesday</b><b> 21:00</b> (BRT)</span>
                     <span><a href="https://soundcloud.com/lucas-rapini">Tadeu Estanislau</a><b>1st</b> & <b>3rd</b> <b>Thursday</b><b> 21:00</b> (BRT)</span>
                     <span><a href="https://soundcloud.com/lucas-rapini">Samuel Bastos</a><b>2nd</b> & <b>4th</b> <b>Tuesday</b><b> 21:00</b> (BRT)</span>
@@ -74,7 +55,7 @@ const App = () => {
                 </div>
             </div>
             <div className="player" id="theplayer">
-                <Player playing={playing} volume={volume}/>
+                <Player />
             </div>
         </div>
     );
--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,72 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './player.css';
 
-const Player = (props) => {
-    const [player, setPlayer] = useState(null);
-
-    useEffect(() => {
-        const options = {
-            width: '100%',
-            height: '100%',
-            autoplay: false,
-            channel: "kojovee",
-        };
-        const newPlayer = new window.Twitch.Player("player", options);
-        newPlayer.setVolume(0.5);
-        newPlayer.pause();
-        setPlayer(newPlayer);
-
-        newPlayer.addEventListener(window.Twitch.Player.OFFLINE, () => {
-            window.postMessage('twitchoffline', window.location.origin);
-        });
-
-        newPlayer.addEventListener(window.Twitch.Player.ONLINE, () => {
-            window.postMessage('twitchonline', window.location.origin);
-        });
-
-        const iframe = document.getElementsByTagName("iframe")[0].contentWindow;
-
-        iframe.navigator.mediaSession.metadata = new window.MediaMetadata({
-            title: 'Besouro Live',
-            artwork: [
-                { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
-                { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-                { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-                { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-                { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-                { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
-            ]
-        });
-
-        iframe.navigator.mediaSession.setActionHandler('play', () => {
-            newPlayer.play();
-        });
-
-        iframe.navigator.mediaSession.setActionHandler('pause', () => {
-            newPlayer.pause();
-        });
-    }, []);
-
-    useEffect(() => {
-        if (!player) {
-            return;
-        }
-        player.setVolume(props.volume);
-    }, [props.volume]);
-
-    useEffect(() => {
-        if (!player) {
-            return;
-        }
-        if (props.playing) {
-            player.play();
-        } else {
-            player.pause();
-        }
-    }, [props.playing]);
-      
+const Player = () => {
     return (
-        <div id="player"></div>
+        <div id="player">
+            <iframe id="iframe" name="iframe" src="https://mixer.com/embed/player/besouroradio?muted=false"></iframe>
+        </div>
     );
 }
 
